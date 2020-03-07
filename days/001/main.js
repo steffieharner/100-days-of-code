794eb41dec187a5b452f4a3411b5bde9d80fc97a
steffieharrner <steffie.harner@gmail.com>
2020-03-07 20:20:47 +0900

    self love text over heart

--- days/001/main.js
+++ days/001/main.js
@@ -1,7 +1,23 @@
 anime({
   targets: "div.box.black",
-  translateX: [
+  translateY: [
     { value: 100, duration: 500 },
     { value: 0, duration: 500 }
-  ]
+  ],
+  rotate: {
+    value: "1turn"
+  },
+  delay: function(el, i, li) {
+    return i * 1000;
+  }
+});
+
+anime({
+  targets: ".heart",
+  // translateY: [{ value: 100, duration: 500 }],
+  // translateX: [{ value: 100, duration: 500 }],
+  loop: true,
+  delay: function(el, i, li) {
+    return i * 1000;
+  }
 });
